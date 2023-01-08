9f0f34a485366bce418666c3b9a3b75443b2274b
Aryan Bhola <aryanbhola31@gmail.com>
2023-01-08 16:54:21 +0530

    4

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -8,11 +8,20 @@
     string public symbol = "AB";
     uint256 public decimals = 18;
     uint256 public totalSupply ;
+    
+// Track Balances
+mapping(address => uint256) public balanceOf;
+// Send tokens
 
- 
  constructor(string memory _name,string memory _symbol,uint256 _totalSupply) {
    name = _name;
    symbol = _symbol;
    totalSupply = _totalSupply * (10**decimals);
+   balanceOf[msg.sender] = totalSupply;
  }
+
+
+
+
+
  }
--- test/Token.js
+++ test/Token.js
@@ -6,11 +6,15 @@ const tokens = (n) => {
 }
 
 describe('Token', ()=>{
-let token
+let token,
+    deployer,
+    accounts
 
 beforeEach(async () => {
     const Token = await ethers.getContractFactory('Token')
     token = await Token.deploy('Aryan Bhola','AB','1000000')
+    accounts = await ethers.getSigners()
+     deployer = accounts[0]
 })
 
 describe('Deployement',() => {
@@ -32,6 +36,11 @@ describe('Deployement',() => {
   it('has correct total Supply', async () => {
     expect(await token.totalSupply()).to.equal(totalSupply)
 })
+it('assigns total supply to deployer', async () => {
+  console.log
+    expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
+})
+
 })
 
 
