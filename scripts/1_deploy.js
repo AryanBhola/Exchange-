87d92bf195a1b8bf457191aa3225626b4a748ae1
Aryan Bhola <aryanbhola31@gmail.com>
2023-01-25 23:10:03 +0530

    14. Seed Exchange

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,13 +1,31 @@
+const { ethers } = require("hardhat")
+
 async function main() {
     // Fetch contract to deploy
     const Token = await ethers.getContractFactory("Token")
-  
+    const Exchange = await ethers.getContractFactory("Exchange")
+
+    //FEtch accounts
+    const accounts = await ethers.getSigners()
+
+
     // Deploy contract
-    const token = await Token.deploy()
-    await token.deployed()
-    console.log(`Token Deployed to: ${token.address}`)
-  }
+    const AB = await Token.deploy('Aryan Bhola','AB','1000000')
+    await AB.deployed()
+    console.log(`AB Deployed to: ${AB.address}`)
   
+    const mETH = await Token.deploy('mETH','mETH','1000000')
+    await mETH.deployed()
+    console.log(`mETH Deployed to: ${mETH.address}`)
+  
+    const mDAI = await Token.deploy('mDAI','mDAI','1000000')
+    await mDAI.deployed()
+    console.log(`mDAI Deployed to: ${mDAI.address}`)
+
+    const exchange = await Exchange.deploy(accounts[1].address, 10)
+    await exchange.deployed()
+    console.log(`Excahnge Deployed to: ${exchange.address}`)
+}
   main()
     .then(() => process.exit(0))
     .catch((error) => {
